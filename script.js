a7842e4e35ad36fd6c7e56af9a79674462a9bac2
gustythefoxy <viberx17@gmail.com>
2024-04-25 19:07:12 +0800

    added

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const sideMenu = document.querySelector('aside');
+const sideMenuC = document.querySelector('#aside_close')
 const menuBtn = document.querySelector('#menu_bar');
 const closeBtn = document.querySelector('#close_btn');
 
@@ -8,12 +9,12 @@ menuBtn.addEventListener('click',()=>{
     sideMenu.style.display = "block"
 })
 closeBtn.addEventListener('click',()=>{
-    sideMenu.style.display = "block"
+    sideMenuC.style.display = "none"
 })
 
 themeToggler.addEventListener('click',()=>{
     document.body.classList.toggle('dark-theme-variables')
 
-    themeToggler.querySelector('span:nth-child(1)').classList.toogle('active')
+    themeToggler.querySelector('span:nth-child(1) ').classList.toogle('active')
     themeToggler.querySelector('span:nth-child(2)').classList.toogle('active')
 })
